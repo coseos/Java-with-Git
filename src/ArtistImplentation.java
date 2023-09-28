890ae50f8d3a407a4b933eb0e39ed83a7c1161f3
Thorsten J. Lorenz <thorsten.lorenz@coseos.com>
2023-09-28 12:49:21 +0200

    add class ArtistImplementation

---
+++ src/ArtistImplentation.java
@@ -0,0 +1,11 @@
+public class ArtistImplentation implements Artist {
+    @Override
+    public void setName(String name) {
+        
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
